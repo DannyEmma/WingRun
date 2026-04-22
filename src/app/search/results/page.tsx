4cb5456a8eb5131317cc724c49946eba327ee7b3
Danny <Danny@macbook-air.home>
2026-04-22 12:19:15 +0400

    Fix some eslint errors

--- src/app/search/results/page.tsx
+++ src/app/search/results/page.tsx
@@ -1,26 +1,27 @@
-import Breadcrumb from '@/components/ui/Breadcrumb/Breadcrumb'
-import styles from './SearchResultsPage.module.css'
-import SneakerItem from '@/components/features/sneaker/SneakerItem/SneakerItem'
-import FilterBar from '@/components/features/category/FilterBar/FilterBar'
-import { BreadcrumbItem } from '@/lib/types'
-import PaginationRounded from '@/components/features/category/PaginationRounded/PaginationRounded'
-import { service } from '@/lib/services'
-import { Audience } from '../../../../prisma/generated/enums'
+import Breadcrumb from "@/components/ui/Breadcrumb/Breadcrumb"
+import styles from "./SearchResultsPage.module.css"
+import SneakerItem from "@/components/features/sneaker/SneakerItem/SneakerItem"
+import FilterBar from "@/components/features/category/FilterBar/FilterBar"
+import { BreadcrumbItem } from "@/lib/types"
+import PaginationRounded from "@/components/features/category/PaginationRounded/PaginationRounded"
+import { service } from "@/lib/services"
+import { Audience } from "../../../../prisma/generated/enums"
 
-export default async function SearchResultsPage({ searchParams }: { searchParams: Record<string, string> }) {
+export default async function SearchResultsPage({ searchParams }: { searchParams: Promise<Record<string, string>> }) {
   const {
     q: query,
     adults: adutlsParam,
     kids: kidsParam,
-    page: currentPage = '1',
+    page: currentPage = "1",
     brands: brandsParams,
     sizes: sizesParams,
     colors: colorsParams,
     priceRange: priceRangeParams,
-    sort: sortParams = 'asc',
+    sort: sortParams = "asc",
   } = await searchParams
 
-  const audiences: Audience[] = (adutlsParam?.split(',').filter((v) => v !== ',') as Audience[]) || (kidsParam?.split(',').filter((v) => v !== ',') as Audience[]) || []
+  const audiences: Audience[] =
+    (adutlsParam?.split(",").filter((v) => v !== ",") as Audience[]) || (kidsParam?.split(",").filter((v) => v !== ",") as Audience[]) || []
 
   const [colorsFilter, pricesRange, sizesList, brandList] = await Promise.all([
     service.color.getColorsFilter().then((res) => res.data),
@@ -29,14 +30,19 @@ export default async function SearchResultsPage({ searchParams }: { searchParams
     service.brand.getAllBrand().then((res) => res.data),
   ])
 
-  const filters = { brands: brandsParams?.split(','), sizes: sizesParams?.split(','), colors: colorsParams?.split(','), priceRange: priceRangeParams?.split(',') }
+  const filters = {
+    brands: brandsParams?.split(","),
+    sizes: sizesParams?.split(","),
+    colors: colorsParams?.split(","),
+    priceRange: priceRangeParams?.split(","),
+  }
 
   const { data, error } = await service.product.getProductsPerPageBySearchQuery({
     page: Number(currentPage),
     searchQuery: query
-      .split(' ')
-      .filter((v) => v !== '')
-      .join(' | '),
+      .split(" ")
+      .filter((v) => v !== "")
+      .join(" | "),
     audiences,
     filters,
     sort: sortParams,
@@ -47,16 +53,16 @@ export default async function SearchResultsPage({ searchParams }: { searchParams
   const pages = data?.pagination.totalPages ?? 0
 
   const breadcrumbItems: BreadcrumbItem[] = [
-    { label: 'WingRun', url: '/' },
+    { label: "WingRun", url: "/" },
     { label: `Résultats pour "${query}"`, url: null },
   ]
 
   return (
-    <main className={styles['category-page']}>
+    <main className={styles["category-page"]}>
       {/* //---------- TITLE  ----------// */}
-      <div className={styles['title-container']}>
-        <div className={styles['title-content']}>
-          <h1 className={styles['title']}>{`"${query.replaceAll('_', ' ')}"`}</h1>
+      <div className={styles["title-container"]}>
+        <div className={styles["title-content"]}>
+          <h1 className={styles["title"]}>{`"${query.replaceAll("_", " ")}"`}</h1>
         </div>
       </div>
 
@@ -68,7 +74,7 @@ export default async function SearchResultsPage({ searchParams }: { searchParams
       )}
 
       {/* //----------  SNEAKERS GRID ----------// */}
-      <div className={styles['sneakers-grid']}>{productPerPage && productPerPage.map((product, index) => <SneakerItem key={index} data={product} />)}</div>
+      <div className={styles["sneakers-grid"]}>{productPerPage && productPerPage.map((product, index) => <SneakerItem key={index} data={product} />)}</div>
 
       {pages > 1 && <PaginationRounded pages={pages} />}
     </main>
