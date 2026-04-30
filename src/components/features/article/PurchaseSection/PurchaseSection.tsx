cae2d9c274ec1a33a9af2e2d1a7e9e106d887579
Danny <Danny@macbook-air.home>
2026-04-30 17:07:28 +0400

    Fix: icons:  paypal, mastercard, visa

--- src/components/features/article/PurchaseSection/PurchaseSection.tsx
+++ src/components/features/article/PurchaseSection/PurchaseSection.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client"
 
-import styles from './PurchaseSection.module.css'
-import { useShoppingCartStore } from '@/lib/stores/shopping-cart.store'
-import Image from 'next/image'
-import { ProductCart, Size } from '@/lib/types'
-import CTA from '@/components/ui/CTA/CTA'
+import styles from "./PurchaseSection.module.css"
+import { useShoppingCartStore } from "@/lib/stores/shopping-cart.store"
+import Image from "next/image"
+import { ProductCart, Size } from "@/lib/types"
+import CTA from "@/components/ui/CTA/CTA"
 
 interface PurchaseSection {
   sneaker: ProductCart
@@ -24,13 +24,13 @@ export default function PurchaseSection({ sneaker, selectedSize }: PurchaseSecti
   }
 
   return (
-    <div className={styles['purchase-section']}>
+    <div className={styles["purchase-section"]}>
       <CTA onClick={handleAddToCart} variant="primary" disabled={!selectedSize}>
         Ajouter au panier
       </CTA>
 
-      <div className={styles['reassurance-container']}>
-        <div className={styles['payement-icons-container']}>
+      <div className={styles["reassurance-container"]}>
+        <div className={styles["payement-icons-container"]}>
           <Image src="/images/ui/icons/paypal-icon.png" alt="Paypal Icon" width={50} height={50} />
           <Image src="/images/ui/icons/visa-icon.png" alt="Visa Icon" width={50} height={50} />
           <Image src="/images/ui/icons/mastercard-icon.png" alt="MasterCard Icon" width={50} height={50} />
