af6ceed410405df3ada20f641fe5f72942371049
Danny <emmadanny@live.fr>
2026-05-15 16:10:11 +0400

    change api version of stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe'
 if (!process.env.STRIPE_SECRET_KEY) throw new Error('Missing env variable STRIPE_SECRET_KEY')
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2026-04-22.dahlia',
+  apiVersion: '2026-02-25.clover',
   typescript: true,
 })
